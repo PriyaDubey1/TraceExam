ae2cc8957e45d0a21a23ca0fb0c1c88bf167aeed
PriyaDubey1 <dubepriya039@gmail.com>
2026-07-27 23:35:05 +0530

    Add stats endpoint for dashboard summary

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const incidentsRoute = require('./routes/incidents');
 const packetsRoute = require('./routes/packets');
 const custodyRoute = require('./routes/custody');
+const statsRoute = require('./routes/stats');
 
 const app = express();
 app.use(cors());
@@ -12,8 +13,10 @@ app.use(express.json());
 app.use('/api/incidents', incidentsRoute);
 app.use('/api/packets', packetsRoute);
 app.use('/api/custody', custodyRoute);
+app.use('/api/stats', statsRoute);
 
 app.get('/', (req, res) => res.send('TraceExam API is running ✅'));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
